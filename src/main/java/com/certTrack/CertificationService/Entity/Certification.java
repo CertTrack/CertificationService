6472685be52f16d33b9b8061d548d5e19f2a5dd7
Dmytro Trofimov <dima6836753@gmail.com>
2025-01-14 09:16:20 +0100

    update logic of issue date and validation code

--- src/main/java/com/certTrack/CertificationService/Entity/Certification.java
+++ src/main/java/com/certTrack/CertificationService/Entity/Certification.java
@@ -1,5 +1,12 @@
 package com.certTrack.CertificationService.Entity;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Base64;
+
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Column;
@@ -7,9 +14,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
 import lombok.ToString;
-
 
 @ToString
 @Entity
@@ -17,69 +24,89 @@ import lombok.ToString;
 public class Certification {
 
 	@JsonIgnore
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
-    private int userId;
-    private int courseId;
-    private String issuedDate;
-    @Column(unique = true, nullable = false)
-    private String validationCode;
-    private String filePath;
-    
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private int userId;
+	private int courseId;
+	private String issuedDate;
+	@Column(unique = true, nullable = false)
+	private String validationCode;
+	private String filePath;
 
-    
-    
-    public int getId() {
-        return id;
-    }
+	@PrePersist
+	public void updateIssueDateAndCreateValidationCode() {
+		LocalDateTime dateTime = LocalDateTime.now();
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+		this.issuedDate = dateTime.format(formatter);
+		
+		generateValidationCode();
+	}
 
-    public void setId(int id) {
-        this.id = id;
-    }
+	public void generateValidationCode() {
+		String combinedString = this.userId + this.courseId + this.issuedDate + this.filePath;
+		this.validationCode = hashString(combinedString);
+	}
 
-    public int getUserId() {
-        return userId;
-    }
+	private static String hashString(String input) {
+		try {
+			MessageDigest digest = MessageDigest.getInstance("SHA-256");
+			byte[] hashBytes = digest.digest(input.getBytes(StandardCharsets.UTF_8));
+			return Base64.getUrlEncoder().withoutPadding().encodeToString(hashBytes);
+		} catch (NoSuchAlgorithmException e) {
+			throw new RuntimeException("Error generating validation code", e);
+		}
+	}
 
-    public void setUserId(int userId) {
-        this.userId = userId;
-    }
+	public int getId() {
+		return id;
+	}
 
-    public int getCourseId() {
-        return courseId;
-    }
+	public void setId(int id) {
+		this.id = id;
+	}
 
-    public void setCourseId(int courseId) {
-        this.courseId = courseId;
-    }
+	public int getUserId() {
+		return userId;
+	}
 
-    public String getIssuedDate() {
-        return issuedDate;
-    }
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
 
-    public void setIssuedDate(String issuedDate) {
-        this.issuedDate = issuedDate;
-    }
+	public int getCourseId() {
+		return courseId;
+	}
 
-    public String getValidationCode() {
-        return validationCode;
-    }
+	public void setCourseId(int courseId) {
+		this.courseId = courseId;
+	}
 
-    public void setValidationCode(String validationCode) {
-        this.validationCode = validationCode;
-    }
+	public String getIssuedDate() {
+		return issuedDate;
+	}
 
-    public String getFilePath() {
-        return filePath;
-    }
+	public void setIssuedDate(String issuedDate) {
+		this.issuedDate = issuedDate;
+	}
 
-    public void setFilePath(String filePath) {
-        this.filePath = filePath;
-    }
+	public String getValidationCode() {
+		return validationCode;
+	}
 
-	public Certification(int userId, int courseId, String issuedDate, String validationCode,
-			String filePath) {
+	public void setValidationCode(String validationCode) {
+		this.validationCode = validationCode;
+	}
+
+	public String getFilePath() {
+		return filePath;
+	}
+
+	public void setFilePath(String filePath) {
+		this.filePath = filePath;
+	}
+
+	public Certification(int userId, int courseId, String issuedDate, String validationCode, String filePath) {
 		this.userId = userId;
 		this.courseId = courseId;
 		this.issuedDate = issuedDate;
@@ -87,6 +114,7 @@ public class Certification {
 		this.filePath = filePath;
 	}
 
-	public Certification() {	}
-    
+	public Certification() {
+	}
+
 }
